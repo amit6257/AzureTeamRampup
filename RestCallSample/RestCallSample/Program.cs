9323357f5dd68609fbe79014ddadcf226a479eed
amaga <amaga@microsoft.com>
2016-11-04 17:27:47 -0700

    Implementation of simple HTTP GET rest call

---
+++ RestCallSample/RestCallSample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestCallSample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string url = "http://stackoverflow.com/questions/27108264/c-sharp-how-to-properly-make-a-http-web-get-request";
+
+            Console.WriteLine("start");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            // default is GET method
+            // request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream resStream = response.GetResponseStream();
+
+            if (resStream != null)
+            {
+                StreamReader reader = new StreamReader(resStream);
+                string res = reader.ReadToEnd();
+                Console.WriteLine(res);
+            }
+
+            Console.WriteLine("end");
+            Console.ReadLine();
+        }
+    }
+}
