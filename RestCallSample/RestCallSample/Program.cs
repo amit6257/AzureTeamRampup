c6f7f2af5475531bc9048ea6d8b630e74c9bfb0b
Amit Agarwal <amaga@microsoft.com>
2016-11-05 23:56:15 -0700

    async call added

--- RestCallSample/RestCallSample/Program.cs
+++ RestCallSample/RestCallSample/Program.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RestCallSample
@@ -14,24 +15,44 @@ namespace RestCallSample
     {
         public static void Main(string[] args)
         {
+            //simpleRestCallSync();
+            simpleRestCallASync();
+            //ThreadingTest.testThreading();
+        }
+
+        private static void simpleRestCallASync()
+        {
+            Console.WriteLine("Starting network async call");
+            ThreadStart ts = new ThreadStart(simpleRestCallSync);
+            Thread t = new Thread(ts);
+            t.Start();
+            Console.WriteLine("End of async call");
+        }
+        
+        // This is a synchronous rest call
+        private static void simpleRestCallSync()
+        {
             string url = "http://stackoverflow.com/questions/27108264/c-sharp-how-to-properly-make-a-http-web-get-request";
 
-            Console.WriteLine("start");
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-
-            // default is GET method
-            // request.Method = "GET";
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Stream resStream = response.GetResponseStream();
-
-            if (resStream != null)
+            Console.WriteLine("starting sync network call");
+            for(int i = 0;i<10;i++)
             {
-                StreamReader reader = new StreamReader(resStream);
-                string res = reader.ReadToEnd();
-                Console.WriteLine(res);
-            }
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
 
-            Console.WriteLine("end");
+                // default is GET method
+                // request.Method = "GET";
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream resStream = response.GetResponseStream();
+
+                if (resStream != null)
+                {
+                    StreamReader reader = new StreamReader(resStream);
+                    string res = reader.ReadToEnd();
+                    //Console.WriteLine(res);
+                }
+            }
+            
+            Console.WriteLine("This is printed at end, i.e it is a sync call");
             Console.ReadLine();
         }
     }
---
+++ RestCallSample/RestCallSample/Threading.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestCallSample
+{
+    class ThreadingTest
+    {
+        public static void testThreading()
+        {
+            ThreadStart threadStart = new ThreadStart(threadMethod);
+            Thread t = new Thread(threadStart);
+            t.Start();
+            Console.WriteLine("after thread start call");
+        }
+        static void threadMethod()
+        {
+            Console.ReadLine();
+            Console.WriteLine("In thread method");
+            Console.ReadLine();
+        }
+    }
+}
