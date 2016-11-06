3d3c8eb5b04e09ada3354f05e84afa345118273a
Amit Agarwal <amaga@microsoft.com>
2016-11-06 01:50:15 -0800

    Adding a TODOController, routing understood

---
+++ WebApiSample/src/WebApiSample/Controllers/TodoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiSample.Models;
+
+namespace WebApiSample.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        public ITodoRepository TodoItems { get; set; }
+
+        public TodoController(ITodoRepository todoItems)
+        {
+            TodoItems = todoItems;
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoItems.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+    }
+}
