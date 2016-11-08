49cf97e91bb283fb8d49939b13831010144d1cf4
amaga <amaga@microsoft.com>
2016-11-07 17:01:03 -0800

    Created a service bus, added a service queue to the bus, this small app sends a message to the queue, see update count in portal, under Free account subscription

---
+++ ServiceBusConsoleApplication/ServiceBusConsoleApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+// help: https://azure.microsoft.com/en-us/documentation/articles/service-bus-dotnet-get-started-with-queues/
+// followed the instructions here, created a subscriptio - Free Trial, created a namespace, create a service Queue,
+// This app sends a message to the queue, which updates Active Message count in the queue, see it in portal.
+namespace ServiceBusConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Endpoint=sb://amaga6257namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=5udu/E3hw/anT7XyloHQRjZ4bZlxH5IeteTs63fnUkk=";
+            var queueName = "amaga6257servicequeue";
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            var message = new BrokeredMessage("This is a test message!");
+            client.Send(message);
+        }
+    }
+}
