72d34e10b20f4a31854a10fac19c66daaf574a96
amaga <amaga@microsoft.com>
2016-11-07 17:13:39 -0800

    Adding a ServiceBusConsoleGetMessageApplication which reads the message from the queue, which was added by the app ServiceBusConsoleApplication

---
+++ ServiceBusConsoleGetMessageApplication/ServiceBusConsoleGetMessageApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+
+// help: https://azure.microsoft.com/en-us/documentation/articles/service-bus-dotnet-get-started-with-queues/
+// Receive messages from the queue
+namespace ServiceBusConsoleGetMessageApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Endpoint=sb://amaga6257namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=5udu/E3hw/anT7XyloHQRjZ4bZlxH5IeteTs63fnUkk=";
+            var queueName = "amaga6257servicequeue";
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+
+            client.OnMessage(message =>
+            {
+                Console.WriteLine(String.Format("Message body: {0}", message.GetBody<String>()));
+                Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
