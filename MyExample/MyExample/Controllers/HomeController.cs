7881a27f7ed595cbf4ef08bfdebb21a208d095be
amaga <amaga@microsoft.com>
2016-12-02 22:09:35 -0800

    Adding commnet

--- MyExample/MyExample/Controllers/HomeController.cs
+++ MyExample/MyExample/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 
 // http://myexample20161202071101.azurewebsites.net/ this is the link
+// https://docs.microsoft.com/en-us/azure/app-service-web/web-sites-dotnet-get-started This is the tutorial for this app
 
 namespace MyExample.Controllers
 {
