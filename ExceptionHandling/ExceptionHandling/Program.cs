a45d13ff84b86effb84ba3578581f1d91360d695
Amit Agarwal <amaga@microsoft.com>
2016-12-09 01:56:41 -0800

    learning exception handling

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            try
+            {
+                p.sequenceCall();
+            }
+            catch (Exception e)
+            {
+                int d = 23;
+
+            }
+            
+        }
+
+        private void sequenceCall()
+        {
+            try
+            {
+                call1();
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            
+           
+
+        }
+
+        private void call1()
+        {
+            try
+            {
+                call2();
+                int d = 23;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("uuuuuu");
+                throw;
+            }
+            
+        }
+
+        private void call2()
+        {
+            try
+            {
+                Console.WriteLine("in exc handlier");
+
+                Console.WriteLine("in exc handlier");
+
+                Console.WriteLine("in exc handlier");
+
+                call3();
+                Console.WriteLine("in ALKNFDKJAF");
+
+                Console.WriteLine("in ======");
+                Console.Read();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("in uauuu");
+                throw;
+                Console.Read();
+            }
+            Console.WriteLine();
+            
+        }
+
+        private void call3()
+        {
+            int d = 0;
+            int ae = d/0;
+        }
+    }
+}
