08ff5f5884e75a818cfecea610d693f45f0db168
Amit Agarwal <amaga@microsoft.com>
2016-12-09 01:55:26 -0800

    creating blobs and contianer in azure storage account

---
+++ AzureBlobStorageConsoleApp/AzureBlobStorageConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Blob; // Namespace for Blob storage types
+
+// https://docs.microsoft.com/en-us/azure/storage/storage-dotnet-how-to-use-blobs
+namespace AzureBlobStorageConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container.
+            CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+
+            // Create the container if it doesn't already exist.
+            container.CreateIfNotExists();
+
+            uploadBlobs();
+
+        }
+
+        private static void uploadBlobs()
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+
+            // Retrieve reference to a blob named "myblob".
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("myblob23");
+
+            // Create or overwrite the "myblob" blob with contents from a local file.
+            using (var fileStream = System.IO.File.OpenRead(@"C:\Users\amaga\Documents\blobsfiles\ss.cs"))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+        }
+    }
+}
