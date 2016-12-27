dcd6f2d85a32388fc5b1c465b359e1e0cdd13d62
amaga <amaga@microsoft.com>
2016-12-26 20:15:30 -0800

    simplest delegate

---
+++ LearnCsharp/LearnCsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LearnCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            learnDelegate();
+        }
+
+        private static void learnDelegate()
+        {
+            Del handler= DelegateMessage;
+            handler("hello , I am a delegate method");
+        }
+
+        private delegate void Del(string mesage);
+        private static void DelegateMessage(string s)
+        {
+            Console.Write(s);
+            Console.Read();
+        }
+    }
+}
