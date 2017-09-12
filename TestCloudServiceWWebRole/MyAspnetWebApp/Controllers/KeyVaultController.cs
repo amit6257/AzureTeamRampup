413095416d0d9725689027a3d705af1aeda547b2
Amit Agarwal <amit6257@gmail.com>
2017-09-11 19:16:34 -0700

    getting the pwd in KeyVaultController

--- TestCloudServiceWWebRole/MyAspnetWebApp/Controllers/KeyVaultController.cs
+++ TestCloudServiceWWebRole/MyAspnetWebApp/Controllers/KeyVaultController.cs
@@ -4,10 +4,19 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using MyAspnetWebApp.AAD;
 
 namespace MyAspnetWebApp.Controllers
 {
     public class KeyVaultController : ApiController
     {
+        public object GetPwdFromKeyVault()
+        {
+            // the secret was pulled during app start.
+            return new
+            {
+                secret = AadUtility.EncryptSecret
+            };
+        }
     }
 }
