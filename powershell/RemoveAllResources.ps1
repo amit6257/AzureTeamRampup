4867d210043225c814854b89197b30b7a526770e
Amit Agarwal <amit6257@gmail.com>
2017-09-12 17:00:09 -0700

    adding powershell folder, remove all resources script

---
+++ powershell/RemoveAllResources.ps1
@@ -0,0 +1,11 @@
+﻿$amit6257VStudioSubId = "c445e683-26aa-4df7-84da-fd2ba8a502e3"
+$context = Get-AzureRmContext
+if($context.Subscription.Id -eq $amit6257VStudioSubId) {
+    $allres = Get-AzureRmResource
+    foreach($res in $allres) {
+        Write-Host "Deleting resource : " $res.Name
+        Remove-AzureRmResource -ResourceId $res.ResourceId -Force
+    }
+} else {
+    Write-Host "Incorrect subscription selected"
+}
